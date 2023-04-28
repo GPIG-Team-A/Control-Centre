3b084a11ca6dc307853294951197ed91eab5b0af
Devon Findlay <devonson4@gmail.com>
2023-04-28 17:36:38 +0100

    Fixing linting

--- digital_twin/rover_rpm_instructions.py
+++ digital_twin/rover_rpm_instructions.py
@@ -2,7 +2,7 @@
 Handles the operations of the motor RPMs
 """
 
-from digital_twin.constants import DISTANCE_BETWEEN_MOTORS, TIME_BETWEEN_MOVEMENTS, WHEEL_CIRCUMFERENCE
+from digital_twin import constants
 
 
 def get_time_for_distance(distance: float, motor_speeds: float) -> float:
@@ -23,7 +23,7 @@ def get_motor_rpm(lin_speed: float) -> float:
     :param lin_speed: The forward linear speed from the motor
     :return: The rotation speed of the motor
     """
-    return lin_speed * 60 / WHEEL_CIRCUMFERENCE
+    return lin_speed * 60 / constants.WHEEL_CIRCUMFERENCE
 
 
 def get_rover_speed(rpm: float) -> float:
@@ -33,11 +33,11 @@ def get_rover_speed(rpm: float) -> float:
     :param rpm:
     :return: The linear velocity of the rover's wheel from its rpm in meters per second
     """
-    return rpm * WHEEL_CIRCUMFERENCE / 60
+    return rpm * constants.WHEEL_CIRCUMFERENCE / 60
 
 
 def angle_from_motor_speed(motor1_speed: float, motor2_speed: float,
-                           time: float = TIME_BETWEEN_MOVEMENTS) -> float:
+                           time: float = constants.TIME_BETWEEN_MOVEMENTS) -> float:
     """
     Calculates the angle rotated by using two motors at the same speed in opposite directions.
 
@@ -51,7 +51,7 @@ def angle_from_motor_speed(motor1_speed: float, motor2_speed: float,
     :param motor2_speed: The linear speed of the second motor in m/s
     :return: The angle rotated by in radians
     """
-    return time * (motor1_speed - motor2_speed) / DISTANCE_BETWEEN_MOTORS
+    return time * (motor1_speed - motor2_speed) / constants.DISTANCE_BETWEEN_MOTORS
 
 
 def get_rpm_for_still_rotation(angle: float, time: float) -> float:
@@ -69,4 +69,4 @@ def get_rpm_for_still_rotation(angle: float, time: float) -> float:
     :param time: The amount of time the rotation will occur
     :return: The absolute RPMs needed for the rotation
     """
-    return get_motor_rpm(angle * DISTANCE_BETWEEN_MOTORS / (2 * time))
+    return get_motor_rpm(angle * constants.DISTANCE_BETWEEN_MOTORS / (2 * time))
